60ea51e1ba9cc5613103d18ef5507b71e95eabbd
Hasan Naqvi <hasannaqvi2007@icloud.com>
2025-10-23 13:34:52 -0400

    Bootcamp part 2 done

--- bootcamp_main.py
+++ bootcamp_main.py
@@ -213,6 +213,7 @@ def main() -> int:
     main_logger.info("Queues cleared")
 
     # Clean up worker processes
+    
 
     # We can reset controller in case we want to reuse it
     # Alternatively, create a new WorkerController instance
